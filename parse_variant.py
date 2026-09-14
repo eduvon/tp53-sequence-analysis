d7ea55292ad47c539af6b4c454339231557deea6
Eric Duvon <eduvon@proton.me>
2026-09-14 14:23:43 -0400

    Separate VCF parsing from variant analysis

--- parse_variant.py
+++ parse_variant.py
@@ -1,16 +1,3 @@
-info = "ALLELEID=347484;CLNDN=Li-Fraumeni_syndrome_1;CLNHGVS=NC_000017.11:g.7668539G>A;CLNSIG=Benign/Likely_benign;GENEINFO=TP53:7157;MC=SO:0001624|3_prime_UTR_variant;ORIGIN=1;RS=114831472"
-
-data =info.split(";")
-
-data_dict = dict(item.split("=", 1) for item in data)
-
-
-# print(f"Clinical significance:", data_dict['CLNSIG'])
-# print(f"Variant type:", data_dict['MC'].split('|')[1])
-# print("dbSNP:", f"rs{data_dict['RS']}")
-
-line = "17	7668434	35555	T	G	.	.	AF_TGP=0.00260;ALLELEID=44228;CLNDISDB=.|MONDO:MONDO:0015356,MeSH:D009386,MedGen:C0027672,Orphanet:140162|MedGen:C3661900|MONDO:MONDO:0013876,MedGen:C3553606,OMIM:614740|Gene:553989,MedGen:C1835398,OMIM:151623,Orphanet:524|MONDO:MONDO:0018875,MedGen:C0085390,OMIM:PS151623,Orphanet:524;CLNDN=TP53-related_disorder|Hereditary_cancer-predisposing_syndrome|not_provided|Basal_cell_carcinoma,_susceptibility_to,_7|Li-Fraumeni_syndrome_1|Li-Fraumeni_syndrome;CLNHGVS=NC_000017.11:g.7668434T>G;CLNREVSTAT=reviewed_by_expert_panel;CLNSIG=Benign;CLNVC=single_nucleotide_variant;GENEINFO=TP53:7157;MC=SO:0001624|3_prime_UTR_variant;ORIGIN=1;RS=78378222"
-
 def parse_vcf_record(line):
     fields = line.split("\t")
 
@@ -20,18 +7,30 @@ def parse_vcf_record(line):
     alt = fields[4]
     info = fields[7]
 
-    data =info.split(";")
+    data = info.split(";")
     data_dict = dict(item.split("=", 1) for item in data)
+
+    consequences = []
+
+    if "MC" in data_dict:
+        for consequence in data_dict["MC"].split(","):
+            description = consequence.split("|")[1]
+            consequences.append(description)
+    else:
+        consequences = [data_dict["CLNVC"]]
+
+    if "RS" in data_dict:
+        rs = f"rs{data_dict['RS']}"
+    else:
+        rs = None
 
     return {
         "chrom": chrom,
         "position": position,
         "ref": ref,
         "alt": alt,
-        "clinical_significance": data_dict['CLNSIG'],
-        "variant_type": data_dict['MC'].split('|')[1],
-        "rs": f"rs{data_dict['RS']}"
+        "clinical_significance": data_dict.get("CLNSIG"),
+        "consequences": consequences,
+        "rs": rs,
+        "genomic_hgvs": data_dict.get("CLNHGVS")
     }
-
-variant = parse_vcf_record(line)
-print(variant)
--- tp53_variants.py
+++ tp53_variants.py
@@ -1,5 +1,6 @@
 import subprocess
 from collections import Counter
+from parse_variant import parse_vcf_record
 
 # Constants
 
@@ -15,43 +16,6 @@ result = subprocess.run(
 )
 
 # Functions
-
-def parse_vcf_record(line):
-    fields = line.split("\t")
-
-    chrom = fields[0]
-    position = int(fields[1])
-    ref = fields[3]
-    alt = fields[4]
-    info = fields[7]
-
-    data = info.split(";")
-    data_dict = dict(item.split("=", 1) for item in data)
-
-    consequences = []
-
-    if "MC" in data_dict:
-        for consequence in data_dict["MC"].split(","):
-            description = consequence.split("|")[1]
-            consequences.append(description)
-    else:
-        consequences = [data_dict["CLNVC"]]
-
-    if "RS" in data_dict:
-        rs = f"rs{data_dict['RS']}"
-    else:
-        rs = None
-
-    return {
-        "chrom": chrom,
-        "position": position,
-        "ref": ref,
-        "alt": alt,
-        "clinical_significance": data_dict.get("CLNSIG"),
-        "consequences": consequences,
-        "rs": rs,
-        "genomic_hgvs": data_dict.get("CLNHGVS"),
-    }
 
 def get_tp53_variants():
     result = subprocess.run(
